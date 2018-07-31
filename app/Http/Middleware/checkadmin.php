63f4836dd7bad6ef1e90ad5d67eb930e302acc06
vincent <phpvincent@163.com>
2018-07-31 15:29:27 +0800

    fix

--- app/Http/Middleware/checkadmin.php
+++ app/Http/Middleware/checkadmin.php
@@ -20,7 +20,7 @@ class checkadmin
 		   return $next($request);
         }
         if($url!='52.14.183.239'||$url!='13.229.201.49'||$url!='127.0.0.1'){
-            return redirect('index/index');
+            return $next($request);
         }
         $msg=preg_match('/^(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]\d|\d)(?:[.](?:25[0-5]|2[0-4]\d|1\d\d|[1-9]\d|\d)){3}$/', $url);
         if(!$msg){
