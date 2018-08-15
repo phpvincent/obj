e986dbec0382e42a1a9a9df1af22714259564ace
vincent <phpvincent@163.com>
2018-08-15 19:37:20 +0800

    ip

--- app/helpers.php
+++ app/helpers.php
@@ -31,7 +31,16 @@ if (!function_exists("getclientcity")) {
     function getclientcity(Request $request)
     { set_time_limit(0);
       $ip=$request->getClientIp();
-        $data = file_get_contents('http://ip.taobao.com/service/getIpInfo.php?ip='.$ip);
+        $data = @file_get_contents('http://ip.taobao.com/service/getIpInfo.php?ip='.$ip);
+        if($data==null||$data==undifined||$data==''){
+        $data = @file_get_contents('http://ip.taobao.com/service/getIpInfo.php?ip='.$ip);          
+        }
+        if($data==null||$data==undifined||$data==''){
+        $data = @file_get_contents('http://ip.taobao.com/service/getIpInfo.php?ip='.$ip);          
+        }
+        if($data==null||$data==undifined||$data==''){
+        $data = @file_get_contents('http://ip.taobao.com/service/getIpInfo.php?ip='.$ip);          
+        }
         $arr=json_decode($data,$assoc=true);
         return $arr['data'];
   }
