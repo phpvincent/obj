2061bcdd89a8442ffdebc6449e5a483447453aa6
vincent <phpvincent@163.com>
2018-09-05 16:46:25 +0800

    video

--- resources/views/home/index2.blade.php
+++ resources/views/home/index2.blade.php
@@ -175,7 +175,7 @@
         </div>
         <div class="detail-block" id="detial-context" style="padding-top:10px">
             @if(!empty($goods->goods_video))
-            <p><video class="edui-upload-video  vjs-default-skin    video-js" controls="" preload="none" width="420" height="280" src="{{$goods->goods_video}}" data-setup="{}"><source src="" type="video/mp4"/></video>
+            <p><video class="edui-upload-video  vjs-default-skin    video-js" controls="" autoplay="autoplay" preload="none" width="420" height="280" src="{{$goods->goods_video}}" data-setup="{}"><source src="" type="video/mp4"/></video>
               
             </p>
             @endif
