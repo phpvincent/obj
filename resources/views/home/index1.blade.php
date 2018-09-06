987ef32e81a3d9ed3a12bfa703df3ba98e7e6aa8
vincent <phpvincent@163.com>
2018-09-06 16:34:29 +0800

    s

--- resources/views/home/index1.blade.php
+++ resources/views/home/index1.blade.php
@@ -65,7 +65,7 @@
         <!-- End Facebook Pixel Code -->
         @endif
           <!-- YaHoo Pixel Code -->
-        @if($goods->goods_pix!=null&&$goods->goods_yahoo_pix!='')
+        @if($goods->goods_yahoo_pix!=null&&$goods->goods_yahoo_pix!='')
         <script type="application/javascript">(function(w,d,t,r,u){w[u]=w[u]||[];w[u].push({'projectId':'10000','properties':{'pixelId':'{{$goods->goods_yahoo_pix}}'}});var s=d.createElement(t);s.src=r;s.async=true;s.onload=s.onreadystatechange=function(){var y,rs=this.readyState,c=w[u];if(rs&&rs!="complete"&&rs!="loaded"){return}try{y=YAHOO.ywa.I13N.fireBeacon;w[u]=[];w[u].push=function(p){y([p])};y(c)}catch(e){}};var scr=d.getElementsByTagName(t)[0],par=scr.parentNode;par.insertBefore(s,scr)})(window,document,"script","https://s.yimg.com/wi/ytc.js","dotq");</script>
         @endif
         <!-- End YaHoo Pixel Code -->
