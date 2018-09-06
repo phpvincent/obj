eaeb219af8e1bf6a6edeaaae7a8147f30a8d13e3
vincent <phpvincent@163.com>
2018-09-06 16:06:04 +0800

    yahoo

--- resources/views/home/buy.blade.php
+++ resources/views/home/buy.blade.php
@@ -123,11 +123,12 @@
         </noscript>
         <!-- End Facebook Pixel Code -->
         @endif
-    <!--雅虎统计代码-->
-   <!--  <script type="application/javascript">(function(w,d,t,r,u){w[u]=w[u]||[];w[u].push({'projectId':'10000','properties':{'pixelId':'10042137'}});var s=d.createElement(t);s.src=r;s.async=true;s.onload=s.onreadystatechange=function(){var y,rs=this.readyState,c=w[u];if(rs&&rs!="complete"&&rs!="loaded"){return}try{y=YAHOO.ywa.I13N.fireBeacon;w[u]=[];w[u].push=function(p){y([p])};y(c)}catch(e){}};var scr=d.getElementsByTagName(t)[0],par=scr.parentNode;par.insertBefore(s,scr)})(window,document,"script","https://s.yimg.com/wi/ytc.js","dotq");</script>
-<noscript>
-  <iframe src="//b.yjtag.jp/iframe?c=FYdC6J1" width="1" height="1" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
-</noscript> -->
+          <!-- YaHoo Pixel Code -->
+        @if($goods->goods_pix!=null&&$goods->goods_yahoo_pix!='')
+        <script type="application/javascript">(function(w,d,t,r,u){w[u]=w[u]||[];w[u].push({'projectId':'10000','properties':{'pixelId':'{{$goods->goods_yahoo_pix}}'}});var s=d.createElement(t);s.src=r;s.async=true;s.onload=s.onreadystatechange=function(){var y,rs=this.readyState,c=w[u];if(rs&&rs!="complete"&&rs!="loaded"){return}try{y=YAHOO.ywa.I13N.fireBeacon;w[u]=[];w[u].push=function(p){y([p])};y(c)}catch(e){}};var scr=d.getElementsByTagName(t)[0],par=scr.parentNode;par.insertBefore(s,scr)})(window,document,"script","https://s.yimg.com/wi/ytc.js","dotq");</script>
+        @endif
+        <!-- End YaHoo Pixel Code -->
+    
 
 </head>
 <body style="">
