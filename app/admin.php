88298f1bdeace658674415bf08a5fd8b099f2e11
vincent <phpvincent@163.com>
2018-09-13 17:04:59 +0800

    fix-vis

--- app/admin.php
+++ app/admin.php
@@ -16,6 +16,7 @@ class admin extends Model implements \Illuminate\Contracts\Auth\Authenticatable
     //获取所在分组的所有管理员id，true为数组形式返回，false为字符串形式返回
     public static function get_group_ids($id,$type=true){
     	$group=self::where('admin_id',$id)->first()['admin_group'];
+
     	if($group==null){
     		if($type){
     			return $arr=[$id];
@@ -23,7 +24,11 @@ class admin extends Model implements \Illuminate\Contracts\Auth\Authenticatable
     			return $str="($id)";
     		}
     	}
-    	$resu=self::where('admin_group',$group)->get(['admin_id'])->toArray();
+        if(\App\admin_group::where('admin_group_id',$group)->first()['admin_group_name']=='#全体人员#'){
+           $resu=self::get(['admin_id'])->toArray();
+        }else{
+           $resu=self::where('admin_group',$group)->get(['admin_id'])->toArray();
+        }
     	if($type){
     		$arr=[];
     		foreach($resu as $k => $v){
