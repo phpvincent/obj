f61e7c45ac8a4cc78bb3a7fb4f817b44e6ed8db9
vincent <phpvincent@163.com>
2018-10-08 19:50:42 +0800

    fix

--- config/paypal.php
+++ config/paypal.php
@@ -23,7 +23,7 @@ return [
 
     'payment_action' => 'Sale', // Can only be 'Sale', 'Authorization' or 'Order'
     'currency'       => 'TWD',
-    'billing_type'   => 'MerchantInitiatedBilling',
+    'billing_type'   => '',
     'notify_url'     => '', // Change this accordingly for your application.
     'locale'         => '', // force gateway language  i.e. it_IT, es_ES, en_US ... (for express checkout only)
     'validate_ssl'   => true, // Validate SSL when creating api client.
