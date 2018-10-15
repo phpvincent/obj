ad14a611e96bfca13300ad8093342cfb0e6856e4
phpvincent@163.com <phpvincent@163.com>
2018-10-15 16:58:46 +0800

    后台商品增加修改页面，自定义套餐满减优惠增加删除功能

--- resources/views/admin/goods/channel2.blade.php
+++ resources/views/admin/goods/channel2.blade.php
@@ -1,23 +1,34 @@
-
+<style>
+	.deletes{
+		font-size:18px;
+		display: inline-block;
+    	padding-left: 10px;
+	}
+</style>
 	<div style="margin:0px auto;border: 1px dashed #000;border-radius: 3%; width: 73%;margin-left:18%; " id="pzhtml">
 		<input type="button" class="btn btn-default" value="增加配置项" id="addpz" style="margin-left:18%;" />
 		<label class="form-label col-xs-4 col-sm-2"> </label>
 		@if($cuxiao!=null)
 							<br/>促销名:<input type="text" style="width: 20%;" class="input-text"  value="{{$cuxiao->cuxiao_msg}}" placeholder="" id="cuxiao_msg" name="cuxiao_msg">
 		@foreach($cuxiao->cuxiao_config as $val => $key)
-		<br/>
-					购满件数:<input type="text" style="width: 10%;" class="input-text" value="{{$key['num']}}" placeholder="" onkeyup="(this.v=function(){this.value=this.value.replace(/[^0-9-]+/,'');}).call(this)" onblur="this.v();" id="cuxiao_num" name="cuxiao_num[{{$val}}]">
-					减免价格:<input type="text" style="width: 10%;" class="input-text" value="{{$key['price']}}" placeholder="" onkeyup="(this.v=function(){this.value=this.value.replace(/[^0-9-]+/,'');}).call(this)" onblur="this.v();" id="cuxiao_prize" name="cuxiao_prize[{{$val}}]">
+		
+					<div>
+						购满件数:<input type="text" style="width: 10%;" class="input-text" value="{{$key['num']}}" placeholder="" onkeyup="(this.v=function(){this.value=this.value.replace(/[^0-9-]+/,'');}).call(this)" onblur="this.v();" id="cuxiao_num" name="cuxiao_num[{{$val}}]">
+						减免价格:<input type="text" style="width: 10%;" class="input-text" value="{{$key['price']}}" placeholder="" onkeyup="(this.v=function(){this.value=this.value.replace(/[^0-9-]+/,'');}).call(this)" onblur="this.v();" id="cuxiao_prize" name="cuxiao_prize[{{$val}}]">
+						<span class="deletes"><i class="Hui-iconfont"></i></span>
+					</div>
 				
-		<br/>
+		
 		
 		@endforeach
 		@else
 							<br/>促销名:<input type="text" style="width: 20%;" class="input-text"  value="" placeholder="" id="cuxiao_msg" name="cuxiao_msg">
-							<br/>
+							
+							<div>
 							购满件数:<input type="text" style="width: 10%;" class="input-text" value="" placeholder="" id="cuxiao_num" onkeyup="(this.v=function(){this.value=this.value.replace(/[^0-9-]+/,'');}).call(this)" onblur="this.v();" name="new_cuxiao[0][num]">
 							减免价格:<input type="text" style="width: 10%;" class="input-text" value="" placeholder="" id="cuxiao_prize" onkeyup="(this.v=function(){this.value=this.value.replace(/[^0-9-]+/,'');}).call(this)" onblur="this.v();" name="new_cuxiao[0][price]">
-							<br/>
+							<span class="deletes"><i class="Hui-iconfont"></i></span>
+							</div>
 		@endif
 	</div>
 
@@ -34,9 +45,12 @@
 					}
 				})
 	var index=1;
-				$("#addpz").on("click",function(){
-					var html='<br/>购满件数:<input type="text" style="width: 10%;" class="input-text" value="" onkeyup="(this.v=function(){this.value=this.value.replace(/[^0-9-]+/,\'\');}).call(this)" onblur="this.v();" placeholder="" id="cuxiao_num" name="new_cuxiao['+index+'][num]">减免价格:<input type="text" style="width: 10%;" class="input-text" value="" onkeyup="(this.v=function(){this.value=this.value.replace(/[^0-9-]+/,\'\');}).call(this)" onblur="this.v();" placeholder="" id="goods_end2" name="new_cuxiao['+index+'][price]"><br/>';
-					$("#pzhtml").append(html);
-					index+=1;
-					})
+	$("#addpz").on("click",function(){
+		var html='<div>购满件数:<input type="text" style="width: 10%;" class="input-text" value="" onkeyup="(this.v=function(){this.value=this.value.replace(/[^0-9-]+/,\'\');}).call(this)" onblur="this.v();" placeholder="" id="cuxiao_num" name="new_cuxiao['+index+'][num]">减免价格:<input type="text" style="width: 10%;" class="input-text" value="" onkeyup="(this.v=function(){this.value=this.value.replace(/[^0-9-]+/,\'\');}).call(this)" onblur="this.v();" placeholder="" id="goods_end2" name="new_cuxiao['+index+'][price]"><span class="deletes"><i class="Hui-iconfont"></i></span></div>';
+		$("#pzhtml").append(html);
+		index+=1;
+	});
+	$('body').on("click",".deletes",function(){
+		$(this).parent().remove();
+	})
 </script>
--- resources/views/admin/goods/channel3.blade.php
+++ resources/views/admin/goods/channel3.blade.php
@@ -1,10 +1,17 @@
-
+<style>
+	.deletes{
+		font-size:18px;
+		display: inline-block;
+    	padding-left: 10px;
+	}
+</style>
 	<div style="margin:0px auto;border: 1px dashed #000;border-radius: 3%; width: 73%;margin-left:18%; " id="pzhtml">
 		<input type="button" class="btn btn-default" value="增加配置项" id="addpz" style="margin-left:18%;" />
 		<label class="form-label col-xs-4 col-sm-2"> </label>
 		@if($cuxiao!=null)
 		@foreach($cuxiao as $key)
-		<br/>
+		
+					<div>
 					促销名:<input type="text" style="width: 10%;" class="input-text" value="{{$key->cuxiao_msg}}" placeholder="" id="cuxiao_msg" name="cuxiao_msg[{{$key->cuxiao_id}}]">
 					件数:<input type="text" style="width: 10%;" class="input-text" value="{{explode(',',$key->cuxiao_config)[0]}}" placeholder="" onkeyup="(this.v=function(){this.value=this.value.replace(/[^0-9-]+/,'');}).call(this)" onblur="this.v();" id="cuxiao_num" name="cuxiao_num[{{$key->cuxiao_id}}]">
 					价格:<input type="text" style="width: 10%;" class="input-text" value="{{explode(',',$key->cuxiao_config)[1]}}" placeholder="" onkeyup="(this.v=function(){this.value=this.value.replace(/[^0-9-]+/,'');}).call(this)" onblur="this.v();" id="cuxiao_prize" name="cuxiao_prize[{{$key->cuxiao_id}}]">
@@ -14,10 +21,12 @@
 							<option value="{{$v->price_id}}"';@if($v->price_id==$key->special_price_id) selected="selected" style="float:right;" @endif  >{{$v->price_name}}</option>
 								@endforeach
 						</select>
-		<br/>
+						<span class="deletes"><i class="Hui-iconfont"></i></span>
+					</div>
+		
 		@endforeach
 		@else
-		<br/>促销名:<input type="text" style="width: 10%;" class="input-text" value="" placeholder="" id="cuxiao_msg" name="new_cuxiao[0][msg]">件数:<input type="text" style="width: 10%;" class="input-text" value="" onkeyup="(this.v=function(){this.value=this.value.replace(/[^0-9-]+/,'');}).call(this)" onblur="this.v();" placeholder="" id="cuxiao_num" name="new_cuxiao[0][num]">价格:<input type="text" style="width: 10%;" class="input-text" value="" onkeyup="(this.v=function(){this.value=this.value.replace(/[^0-9-]+/,'');}).call(this)" onblur="this.v();" placeholder="" id="goods_end2" name="new_cuxiao[0][price]">	赠品:<select name="new_cuxiao[][free]" class="select slectchange" style="width:30%;" ><option value="0" >无</option>@foreach(\App\price::get() as $v)<option value="{{$v->price_id}}">{{$v->price_name}}</option> @endforeach </select><br/>
+		<div>促销名:<input type="text" style="width: 10%;" class="input-text" value="" placeholder="" id="cuxiao_msg" name="new_cuxiao[0][msg]">件数:<input type="text" style="width: 10%;" class="input-text" value="" onkeyup="(this.v=function(){this.value=this.value.replace(/[^0-9-]+/,'');}).call(this)" onblur="this.v();" placeholder="" id="cuxiao_num" name="new_cuxiao[0][num]">价格:<input type="text" style="width: 10%;" class="input-text" value="" onkeyup="(this.v=function(){this.value=this.value.replace(/[^0-9-]+/,'');}).call(this)" onblur="this.v();" placeholder="" id="goods_end2" name="new_cuxiao[0][price]">	赠品:<select name="new_cuxiao[][free]" class="select slectchange" style="width:30%;" ><option value="0" >无</option>@foreach(\App\price::get() as $v)<option value="{{$v->price_id}}">{{$v->price_name}}</option> @endforeach </select><span class="deletes"><i class="Hui-iconfont"></i></span></div>
 		@endif
 	</div>
 
@@ -35,8 +44,11 @@
 				})
 	var index=1;
 				$("#addpz").on("click",function(){
-					var html='<br/>促销名:<input type="text" style="width: 10%;" class="input-text" value="" placeholder="" id="cuxiao_msg" name="new_cuxiao['+index+'][msg]">件数:<input type="text" style="width: 10%;" onkeyup="(this.v=function(){this.value=this.value.replace(/[^0-9-]+/,\'\');}).call(this)" onblur="this.v();" class="input-text" value="" placeholder="" id="cuxiao_num" name="new_cuxiao['+index+'][num]">价格:<input type="text" style="width: 10%;" onkeyup="(this.v=function(){this.value=this.value.replace(/[^0-9-]+/,\'\');}).call(this)" onblur="this.v();" class="input-text" value="" placeholder="" id="goods_end2" name="new_cuxiao['+index+'][price]">	赠品:<select name="new_cuxiao['+index+'][free]" class="select slectchange" style="width:30%;" ><option value="0" >无</option>@foreach(\App\price::get() as $v)<option value="{{$v->price_id}}">{{$v->price_name}}</option> @endforeach </select><br/>';
+					var html='<div>促销名:<input type="text" style="width: 10%;" class="input-text" value="" placeholder="" id="cuxiao_msg" name="new_cuxiao['+index+'][msg]">件数:<input type="text" style="width: 10%;" onkeyup="(this.v=function(){this.value=this.value.replace(/[^0-9-]+/,\'\');}).call(this)" onblur="this.v();" class="input-text" value="" placeholder="" id="cuxiao_num" name="new_cuxiao['+index+'][num]">价格:<input type="text" style="width: 10%;" onkeyup="(this.v=function(){this.value=this.value.replace(/[^0-9-]+/,\'\');}).call(this)" onblur="this.v();" class="input-text" value="" placeholder="" id="goods_end2" name="new_cuxiao['+index+'][price]">	赠品:<select name="new_cuxiao['+index+'][free]" class="select slectchange" style="width:30%;" ><option value="0" >无</option>@foreach(\App\price::get() as $v)<option value="{{$v->price_id}}">{{$v->price_name}}</option> @endforeach </select><span class="deletes"><i class="Hui-iconfont"></i></span></div>';
 					$("#pzhtml").append(html);
 					index+=1;
 					})
+	$('body').on("click",".deletes",function(){
+		$(this).parent().remove();
+	})
 </script>
