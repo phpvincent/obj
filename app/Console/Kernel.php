1e2871dab003a6d2cfb02d4ccd3b882f4079ce83
vincent <phpvincent@163.com>
2018-10-19 20:03:21 +0800

    order-time

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,12 +26,48 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')
         //          ->hourly();
-               /*  $schedule->call(function(){
-                    \App\order::where(function($query){
+                 $filePath='./time.log';
+                 $schedule->call(function(){
+                    $maxtime=2400;
+                    $herbmaster=\App\herbmaster::where('herbmaster_type','0')->first();
+                    if($herbmaster==null||strtotime($herbmaster['herbmaster_msg'])==false||strtotime($herbmaster['herbmaster_msg'])<time()-2592000){
+                        $order_ltime=date("Y-m-d H:i:s",time()-2592000);
+                    }else{
+                        $order_ltime=$herbmaster['herbmaster_msg'];
+                    }
+                    $orders=\App\order::where(function($query){
                         $query->where('order_pay_type','1');
-                        
-                    })->all();
-                 })->everyMinute()->evenInMaintenanceMode()->appendOutputTo($filePath);*/
+                        $query->where('order_time','>',$order_ltime);
+                    })
+                    ->orderBy('order_time','asc');
+                    ->all();
+                    $new_time='';
+                    foreach($orders as $k => $v){
+                        $order_time=$v->order_time;
+                        if(time()-strtotime($order_time)>=2400&&$v->order_type==9){
+                            $new_time=$v->order_time;
+                            $msg=$v->update(['is_del'=>'1']);
+                            if($msg){
+                                \Log::notice($v->order_id.'号订单预付款订单超时'.$maxtime.'被删除')；
+                            }else{
+                                \Log::notice($v->order_id."号订单删除失败");
+                            }
+                        }
+                        if($new_time==''){
+                            if($herbmaster==null){
+                                $newherbmaster=new \App\herbmaster();
+                                $newherbmaster->herbmaster_type=null;
+                                $newherbmaster->save();
+                            }else{
+                                $herbmaster->herbmaster_msg=date("Y-m-d H:i:s",time());
+                                $herbmaster->save();
+                            }
+                        }else{
+                            $herbmaster->herbmaster_msg=$new_time;
+                            $herbmaster->save();
+                        }
+                    }
+                 })->everyMinute()->evenInMaintenanceMode()->appendOutputTo($filePath);
     }
 
     /**
---
+++ app/herbmaster.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class herbmaster extends Model
+{
+    protected $table = 'herbmaster';
+    protected $primaryKey ='herbmaster_id';
+    public $timestamps=false;
+}
