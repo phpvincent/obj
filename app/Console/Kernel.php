650bc95cc972f642afea6e17edfa82d8a47aac9e
vincent <phpvincent@163.com>
2018-10-24 20:00:10 +0800

    fix

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -34,7 +34,7 @@ class Kernel extends ConsoleKernel
                  $schedule->call(function(){
                     //get_new_currency_rate();
                     \Log::notice('check');
-                 })->dailyAt('19:57');
+                 })->dailyAt('19:59');
     }
     /**
      * Register the commands for the application.
