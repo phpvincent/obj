63c1b693304f2201fc9f37c50ebfc8343f734fa9
vincent <phpvincent@163.com>
2018-10-25 20:16:42 +0800

    fix

--- app/Jobs/SendHerbEmail.php
+++ app/Jobs/SendHerbEmail.php
@@ -8,6 +8,7 @@ use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use App\order;
+use App\url;
 class SendHerbEmail implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
