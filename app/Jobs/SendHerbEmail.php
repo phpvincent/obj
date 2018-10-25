b8dc2e3edea639eb98c4c0d19e1725735988ecd5
vincent <phpvincent@163.com>
2018-10-25 18:03:57 +0800

    server

---
+++ app/Jobs/SendHerbEmail.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+
+class SendHerbEmail implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    protected $order;
+    public function __construct(Order $order)
+    {
+        $this->order=$order;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+         $order=$this->order;
+       $flag = \Mail::raw('server test'.$order->id,function($message){
+            $to = 'wxhwxhwxh@qq.com';
+            $message ->to('wxhwxhwxh@qq.com')->subject('order notice');
+        });
+       
+        if(\Mail::failures()==[]){
+            \Log::notice('发送邮件成功，请查收！');
+        }else{
+            \Log::notice('发送邮件失败，请重试！');
+        }
+    }
+}
