0575ee27c415715040d16016924ea361011817f5
vincent <phpvincent@163.com>
2018-10-27 18:40:41 +0800

    fix

--- resources/views/admin/order/index.blade.php
+++ resources/views/admin/order/index.blade.php
@@ -215,7 +215,7 @@
     backButton.on('click', backToTop);
  
     $(window).on('scroll', function () {/*当滚动条的垂直位置大于浏览器所能看到的页面的那部分的高度时，回到顶部按钮就显示 */
-        if ($(window).scrollTop() > $(window).height())
+        if ($(window).scrollTop() > $(window).height()-300)
             backButton.fadeIn();
         else
             backButton.fadeOut();
