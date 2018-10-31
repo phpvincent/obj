146803375ed1f31dd1e3f722b7520a8a56518f20
phpvincent@163.com <phpvincent@163.com>
2018-10-31 21:35:01 +0800

    修改邮箱模板

--- resources/views/home/TaiwanJian/sendmail.blade.php
+++ resources/views/home/TaiwanJian/sendmail.blade.php
@@ -69,20 +69,20 @@
               font-size: 14px;
               line-height: 28px;">
                 <div style="width:70%;margin:0 auto;    text-align: center;">
-                    <p style="margin:0"><font style="vertical-align: inherit;">订单创建时间：{{$order->order_time}}</font></p>
+                    <p style="margin:0"><font style="vertical-align: inherit;">订单创建时间 : {{$order->order_time}}</font></p>
                     <!-- <p style="margin:0"><span style="border-bottom:1px dashed #ccc;" t="5" times=""><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">{{$order->order_time}}</font></font></span></p> -->
-                    <p style="margin:0"><font style="vertical-align: inherit;">支付方式：@if($order->order_pay_type=='0')货到付款@else在线支付@endif</font></p>
-                    <p style="margin:0"><font style="vertical-align: inherit;">备注：@if($order->order_remark!=null){{$order->order_remark}}@endif</font></p>
+                    <p style="margin:0"><font style="vertical-align: inherit;">支付方式 : @if($order->order_pay_type=='0')货到付款@else在线支付@endif</font></p>
+                    <p style="margin:0"><font style="vertical-align: inherit;">备注 : @if($order->order_remark!=null){{$order->order_remark}}@endif</font></p>
                 </div>
                 <div style="width:70%;margin:0 auto;    text-align: center;">
-                  <p style="margin:0">收货地区:&nbsp;{{$order->order_state}} {{$order->order_city}}</p>
-                  <p style="margin:0">详细地址:&nbsp;{{$order->order_add}}</p>
-                  <p style="margin:0">联系电话:&nbsp;{{$order->order_tel}} </p>
+                  <p style="margin:0">收货地区 :&nbsp;{{$order->order_state}} {{$order->order_city}}</p>
+                  <p style="margin:0">详细地址 :&nbsp;{{$order->order_add}}</p>
+                  <p style="margin:0">联系电话 :&nbsp;{{$order->order_tel}} </p>
                   @if($order->order_email!=null)
-                  <p style="margin:0">联系邮箱:{{$order->order_email}}</p>
+                  <p style="margin:0">联系邮箱 : {{$order->order_email}}</p>
                   @endif
                   @if($order->order_zip!=null)
-                  <p style="margin:0">ZIP:&nbsp;{{$order->order_zip}} </p>
+                  <p style="margin:0">ZIP :&nbsp;{{$order->order_zip}} </p>
                   @endif
               </div>
               </td>
@@ -91,7 +91,7 @@
             <td height="3" colspan="3" background=""></td>
           </tr>
           <tr>
-            <td height="20" colspan="3" style=" border-bottom: 1px solid #F29400; color: #F29400;"><p><strong><font style="vertical-align: inherit;">订单详细信息</font></strong></p></td>
+            <td height="20" colspan="3" style=" border-top: 1px solid #F29400; color: #F29400;"><p style="    text-align: center;"><strong><font style="vertical-align: inherit;">产品详细信息</font></strong></p></td>
           </tr>
           <!-- <tr>
             <td height="20" colspan="3"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">订单号：</font></font><span style="border-bottom:1px dashed #ccc;z-index:1" t="7" onclick="return false;" data="{{$order->order_single_id}}"><font style="vertical-align: inherit;"><font style="vertical-align: inherit; color: red;">{{$order->order_single_id}}</font></font></span></td>
@@ -100,41 +100,18 @@
             <td height="20" colspan="3"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">订单创建时间：</font></font><span style="border-bottom:1px dashed #ccc;" t="5" times=""><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">{{$order->order_time}}</font></font></span></td>
           </tr> -->
           <tr>
-            <td colspan="3"    style=" padding: 20px 0 0 20px;">
+            <td colspan="3"    style=" padding: 20px 0 0 20px;color:#666;font-size:14px">
                 <table width="100%" border="0" align="center" cellpadding="0" cellspacing="2">
                                 <tbody><tr>
                         <td width="100" height="100"  style="vertical-align: top;"><div align="center"><a href="{{$url->url_url}}"><img src="{{$goods->img}}" width="100" height="100"></a></div></td>
                         <td width="585" colspan="2" style=" padding-left: 20px;">
-                            <p style="margin:6px 0 10px 0; "><font style="vertical-align: inherit;color:#666;font-size:14px"><font style="vertical-align: inherit;">{{$goods->goods_name}}</font></font></p>
+                            <p style="margin:6px 0 10px 0; "><font style="vertical-align: inherit;color:#333"><font style="vertical-align: inherit;">{{$goods->goods_name}}</font></font></p>
+                            
+                            @if($order->order_cuxiao_id!='暂无促销信息')
                             <p style="margin: 0; ">
-                              
+                              促销活动：{{$order->order_cuxiao_id}}
                             </p>
-                               
-                            @if($order->config_msg!=null)
-                            <div>                
-                              @foreach($order->config_msg as $k => $v)        
-                                <p class="STYLE1">
-                                        <strong>item.{{$k}}</strong><br>
-                                        @foreach($v as $key => $val)
-                                    <font style="vertical-align: inherit;">                              
-                                    {{$val}}</font>@if($key!=count($v)-1)<span>&nbsp;/</span>@endif
-                                        @endforeach
-                                        <br>
-                                </p>
-                              @endforeach
-                            </div>
-                            @endif 
-                                <p>
-                                    <font style="color:red">{{$order->order_price}}</font> <br>
-                                             联系邮箱：
-                                
-                              <a href="mailto:hyfhdcjn@gmail.com"  >hyfhdcjn@gmail.com</a>
-                                </p>
-                                @if($order->order_cuxiao_id!='暂无促销信息')
-                                <p>
-                                  促销活动：{{$order->order_cuxiao_id}}
-                                </p>
-                                @endif
+                            @endif
                         </td>
                     </tr>
                             </tbody></table></td>
@@ -143,7 +120,7 @@
             <td height="3" colspan="3" background=""></td>
           </tr>
           <tr>
-            <td colspan="3">
+            <td colspan="3"style=" padding: 20px 0 0 20px;color:#666;font-size:12px">
                   @if($order->config_msg!=null)
                   <div>                
                     @foreach($order->config_msg as $k => $v)        
@@ -161,23 +138,44 @@
             </td>
           </tr>
           <tr>
-            <td height="40" colspan="3"><div align="right">
-              <p align="right"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">件数：<font style="color:red">{{$order->order_num}}</font> </font></font><br><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">
-                  订单价格：   <font style="color:red">{{$order->order_price}}</font>        </font></font><br><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">
-                  小计：<font style="color:red">{{$order->order_price}}</font></font></font></p>
-            </div></td>
+            <td height="40" colspan="3">
+              <div align="right"style=" padding: 20px 0 0 20px;color:#666;font-size:14px;margin-bottom:30px">
+                <!-- <p align="right">
+                    件数：<font style="color:red">{{$order->order_num}}</font> </font></font><br><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">
+                    订单价格：   <font style="color:red">{{$order->order_price}}</font>        </font></font><br><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">
+                    小计：<font style="color:red">{{$order->order_price}}</font></font>
+                </p> -->
+                <div style="text-align: right;">
+                  <p style="display:inline-block;margin: 6px 0;">单价 :</p><p style="display:inline-block;width: 100px;;margin: 6px 0;text-align: right; padding-left: 4px">asdas</p>
+                </div>
+                <div style="text-align: right;">
+                  <p style="display:inline-block;margin: 6px 0;">件数 :</p><p style="display:inline-block;width: 100px;;margin: 6px 0;text-align: right;padding-left: 4px">213</p>
+                </div>
+                <div style="text-align: right;">
+                  <p style="display:inline-block;margin: 6px 0;">优惠价格 :</p><p style="display:inline-block;width: 100px;;margin: 6px 0;text-align: right;padding-left: 4px">321</p>
+                </div>
+                <div style="text-align: right;">
+                  <p style="display:inline-block;margin: 6px 0;">免费送货 :</p><p style="display:inline-block;width: 100px;;margin: 6px 0;text-align: right;padding-left: 4px">3213</p>
+                </div>
+                <div style="text-align: right;">
+                  <p style="display:inline-block;margin: 6px 0;">总计 :</p><p style="display:inline-block;width: 100px;;margin: 6px 0;text-align: right;padding-left: 4px">321312</p>
+                </div>
+              </div>
+            </td>
           </tr>
           <tr>
-            <td height="40" colspan="3"><p><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">此电子邮件仅用于确认您在</font></font><a><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">{{$url->url_url}}</font></font></a><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">上的 
+            <td height="40" colspan="3"style="color:#666;font-size:14px;border-top: 1px solid #F29400;"><p style="
+              margin-top: 48px;
+          "><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">此电子邮件仅用于确认您在</font></font><a><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">{{$url->url_url}}</font></font></a><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">上的 
                  </font><a><font style="vertical-align: inherit;">订单</font></a><font style="vertical-align: inherit;">。</font></font><br><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">
-                这是一个自动回复电子邮件，你不必回复。</font><font style="vertical-align: inherit;">如果您还有其他问题，请发送电子邮件至 
+                这是一个自动回复电子邮件，您不必回复。</font><font style="vertical-align: inherit;">如果您还有其他问题，请发送电子邮件至 
         
                  </font></font><a href="mailto:hyfhdcjn@gmail.com" style="color:#F8770E">hyfhdcjn@gmail.com</a><font style="vertical-align: inherit;"><font style="vertical-align: inherit;"> 
         
                 。</font></font></p></td>
           </tr>
           <tr>
-            <td height="40" colspan="3"><p><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">期待您的下次访问。</font></font><br>
+            <td height="40" colspan="3"style="color:#666;font-size:14px"><p><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">期待您的下次访问。</font></font><br>
             <a href="mailto:hyfhdcjn@gmail.com" ><font  style="vertical-align: inherit;"><font style="vertical-align: inherit;">{{$url->url_url}}</font></font></a>
             </p></td>
           </tr>
