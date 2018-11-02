cd2dcf166e674775a688275d33f892e9e934408b
phpvincent@163.com <phpvincent@163.com>
2018-11-02 11:24:14 +0800

    修改后台增加单品修改单品页面

--- config/database.php
+++ config/database.php
@@ -61,7 +61,7 @@ return [
         ],
 
         'pgsql' => [
-            'driver' => 'pgsql',
+            'driver' => 'mysql',
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '5432'),
             'database' => env('DB_DATABASE', 'forge'),
