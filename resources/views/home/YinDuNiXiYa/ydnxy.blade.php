6951416a19e966dea0b8715b8ea7782c0ad571cd
phpvincent@163.com <phpvincent@163.com>
2018-11-12 15:59:56 +0800

    修改印尼模板翻译

--- resources/views/home/YinDuNiXiYa/ydnxy.blade.php
+++ resources/views/home/YinDuNiXiYa/ydnxy.blade.php
@@ -231,12 +231,12 @@
 <ul class="detail-bars">
     @if(in_array('introduce',$templets))
     <li>
-        <span href="#detial-context" class="scrollBar" scroll-y="0">Penjelasan produk</span>
+        <span href="#detial-context" class="scrollBar" scroll-y="0">Penjelasan</span>
     </li>
     @endif
     @if(in_array('specifications',$templets))
     <li>
-        <span href="#detial-params" class="scrollBar" scroll-y="50">Spesifikasi produk</span>
+        <span href="#detial-params" class="scrollBar" scroll-y="50">Spesifikasi</span>
     </li>
     @endif
     @if(in_array('evaluate',$templets))
