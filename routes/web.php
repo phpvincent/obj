236575650f62909354ef68686269be240bbae598
vincent <phpvincent@163.com>
2018-11-20 15:27:22 +0800

    fix

--- routes/web.php
+++ routes/web.php
@@ -18,15 +18,15 @@ use App\Jobs\SendHerbEmail;
 	Route::get('/index/index','home\IndexController@channelindex')->name('index');
 	Route::get('/index/fb','home\IndexController@fb');
 /*	Route::get('/index/sendemail','home\IndexController@sendmail');*/
-/*	Route::any('/paypal',function(Request $request){
-
+	Route::any('/paypal',function(Request $request){
+		\Log::notice(json_encode($request->all()).'回调~~~~~~~~~~~~~~~~~~~~~~~~');return;
 		 $url=$_SERVER['SERVER_NAME'];
         $goods=\App\goods::where('goods_id',43)->first();
         $order=\App\order::where('order_id','223')->first();
         return view('home.YinDuNiXiYa.sendmail')->with(compact('url','order','goods'));
 		
 		//SendHerbEmail::dispatch($order);
-	});*/
+	});
 Route::middleware(['checkbus','checkurl'])->group(function(){
 	Route::get('/','home\IndexController@index');
 	/*Route::get('/{rand}','home\IndexController@index');*/
