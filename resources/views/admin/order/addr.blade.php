74195066c3c8a14e49ca292843b1c2be24849ac5
vincent <phpvincent@163.com>
2018-12-20 16:04:43 +0800

    fix

--- resources/views/admin/order/addr.blade.php
+++ resources/views/admin/order/addr.blade.php
@@ -24,6 +24,10 @@
 				<td>{!!$order->order_email==''?"<span style='color:red'>没有填写</span>":$order->order_email!!}</td>
 			</tr>
 			<tr class="text-c">
+				<td>村镇</td>
+				<td>{!!$order->order_village==''?"<span style='color:red'>没有填写</span>":$order->order_village!!}</td>
+			</tr>
+			<tr class="text-c">
 				<td>城市</td>
 				<td>{!!$order->order_city==''?"<span style='color:red'>没有填写</span>":$order->order_city!!}</td>
 			</tr>
@@ -31,6 +35,7 @@
 				<td>地区</td>
 				<td>{!!$order->order_state==''?"<span style='color:red'>没有填写</span>":$order->order_state!!}</td>
 			</tr>
+			
 			<tr class="text-c">
 				<td>详细信息</td>
 				<td>{!!$order->order_add==''?"<span style='color:red'>没有填写</span>":$order->order_add!!}</td>
