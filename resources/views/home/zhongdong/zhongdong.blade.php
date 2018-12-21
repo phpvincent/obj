26ef25f64a7853225907fbf8f0cf7078cc696eee
vincent <phpvincent@163.com>
2018-12-21 12:04:04 +0800

    test

--- resources/views/home/zhongdong/zhongdong.blade.php
+++ resources/views/home/zhongdong/zhongdong.blade.php
@@ -8,7 +8,7 @@
     var js, fjs = d.getElementsByTagName(s)[0];
     if (d.getElementById(id)) return;
     js = d.createElement(s); js.id = id;
-    js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js#xfbml=1&version=v2.12&autoLogAppEvents=1';  FB.CustomerChat.show(true);FB.CustomerChat.showDialog();
+    js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js#xfbml=1&version=v2.12&autoLogAppEvents=1'; 
     fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));
 </script>
@@ -18,5 +18,14 @@
      logged_out_greeting="How can we help you shop today?"
      theme_color="#5c9165">
 </div>
+<script type="text/javascript">
+    window.onload=function (){
+
+    var userName=”xiaoming”;
+
+    alert(userName);
+ FB.CustomerChat.show(true);FB.CustomerChat.showDialog();
+}
+</script>
 </body>
 </html>
