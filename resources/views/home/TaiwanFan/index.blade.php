4ec11b6c310c895ff06a1034e3d76cb901c75907
vincent <phpvincent@163.com>
2018-12-26 10:31:41 +0800

    zongjin

--- resources/views/home/TaiwanFan/index.blade.php
+++ resources/views/home/TaiwanFan/index.blade.php
@@ -117,19 +117,6 @@
         </style>
 	</head>
 	<body style="position:relative">
-        <script type="text/javascript">
-            //宗进
-        window._pt_lt = new Date().getTime();
-        window._pt_sp_2 = [];
-        _pt_sp_2.push('setAccount,4e86cd08');
-        var _protocol = (("https:" == document.location.protocol) ? " https://" : " http://");
-        (function() {
-            var atag = document.createElement('script'); atag.type = 'text/javascript'; atag.async = true;
-            atag.src = _protocol + 'js.ptengine.cn/4e86cd08.js';
-            var s = document.getElementsByTagName('script')[0];
-            s.parentNode.insertBefore(atag, s);
-                })();
-        </script>
 	<script>
 	jQuery(function(){
 	   // jQuery.get('/index/swt',function(html){
