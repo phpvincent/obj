48a70a85f831dd45781fab3b3d06512835de3bd8
zhang <18238205232@163.com>
2019-01-14 16:12:03 +0800

    cui 01-14

--- app/message.php
+++ app/message.php
@@ -49,51 +49,58 @@ class message extends Model
         switch ($blade_id) {
             case '0': //台湾模板
                 if('886' == substr($phone,0,3)){
-                    $phones = $phone;
+                    $tel = substr($phone,4);
+                    $phones = '886'.ltrim($tel,'0');
                 }else{
-                    $phones = '886'.$phone;
+                    $phones = '886'.ltrim($phone,'0');
                 }
                 break;
             case '1': //简体模板
                 if('886' == substr($phone,0,3)){
-                    $phones = $phone;
+                    $tel = substr($phone,4);
+                    $phones = '886'.ltrim($tel,'0');
                 }else{
-                    $phones = '886'.$phone;
+                    $phones = '886'.ltrim($phone,'0');
                 }
                 break;
             case '2': //阿联酋模板
                 if('971' == substr($phone,0,3)){
-                    $phones = $phone;
+                    $tel = substr($phone,4);
+                    $phones = '971'.ltrim($tel,'0');
                 }else{
-                    $phones = '971'.$phone;
+                    $phones = '971'.ltrim($phone,'0');
                 }
                 break;
             case '3': //马来西亚
                 if('60' == substr($phone,0,2)){
-                    $phones = $phone;
+                    $tel = substr($phone,3);
+                    $phones = '60'.ltrim($tel,'0');
                 }else{
-                    $phones = '60'.$phone;
+                    $phones = '60'.ltrim($phone,'0');
                 }
                 break;
             case '4': //泰国
                 if('66' == substr($phone,0,2)){
-                    $phones = $phone;
+                    $tel = substr($phone,3);
+                    $phones = '66'.ltrim($tel,'0');
                 }else{
-                    $phones = '66'.$phone;
+                    $phones = '66'.ltrim($phone,'0');
                 }
                 break;
             case '5': //日本
                 if('81' == substr($phone,0,2)){
-                    $phones = $phone;
+                    $tel = substr($phone,3);
+                    $phones = '81'.ltrim($tel,'0');
                 }else{
-                    $phones = '81'.$phone;
+                    $phones = '81'.ltrim($phone,'0');
                 }
                 break;
             case '6': //印度尼西亚
                 if('62' == substr($phone,0,2)){
-                    $phones = $phone;
+                    $tel = substr($phone,3);
+                    $phones = '62'.ltrim($tel,'0');
                 }else{
-                    $phones = '62'.$phone;
+                    $phones = '62'.ltrim($phone,'0');
                 }
                 break;
             case '7': //菲律宾
@@ -106,30 +113,34 @@ class message extends Model
                 break;
             case '8': //英国
                 if('44' == substr($phone,0,2)){
-                    $phones = $phone;
+                    $tel = substr($phone,3);
+                    $phones = '44'.ltrim($tel,'0');
                 }else{
-                    $phones = '44'.$phone;
+                    $phones = '44'.ltrim($phone,'0');
                 }
                 break;
             case '9': //英国
                 if('44' == substr($phone,0,2)){
-                    $phones = $phone;
+                    $tel = substr($phone,3);
+                    $phones = '44'.ltrim($tel,'0');
                 }else{
-                    $phones = '44'.$phone;
+                    $phones = '44'.ltrim($phone,'0');
                 }
                 break;
             case '10': //美国
                 if('1' == substr($phone,0,1)){
-                    $phones = $phone;
+                    $tel = substr($phone,2);
+                    $phones = '1'.ltrim($tel,'0');
                 }else{
-                    $phones = '1'.$phone;
+                    $phones = '1'.ltrim($phone,'0');
                 }
                 break;
             case '11': //越南
                 if('84' == substr($phone,0,2)){
-                    $phones = $phone;
+                    $tel = substr($phone,3);
+                    $phones = '84'.ltrim($tel,'0');
                 }else{
-                    $phones = '84'.$phone;
+                    $phones = '84'.ltrim($phone,'0');
                 }
                 break;
             default: //沙特、卡塔尔、中东地区
