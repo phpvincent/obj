93240a509597dc63af20cfd9f31d224c64d8e178
fangaige <251160876@qq.com>
2019-01-30 09:56:42 +0800

    懒加载ajax

--- resources/views/home/ydzshome/index.blade.php
+++ resources/views/home/ydzshome/index.blade.php
@@ -38,18 +38,18 @@
 <div class="djs">
 <div class="djstu1">
 <a href="/yundongxie.html">
-<img src="picture/jop.jpg" width="308" height="380">
+<img src="img/zlt.jpg" width="308" height="380">
 </a>
 </div>
 <div class="djstu2">
 <div class="djs01">
 <a href="/best-selling.html">
-<img src="picture/jop2.jpg" width="308" height="190">
+<img src="img/zlt.jpg" width="308" height="190">
 </a>
 </div>
 <div class="djs02">
 <a href="/new-product.html ">
-<img src="picture/jop1.jpg" width="308" height="190">
+<img src="img/zlt.jpg" width="308" height="190">
 </a>
 </div>
 </div>
@@ -61,13 +61,13 @@
 </div>
 </div>
 <div class="new-sale-big">
-<a href="/tooxie.html"><img src="picture/xinpin.jpg" /></a>
+<a href="/tooxie.html"><img src="img/zlt.jpg" /></a>
 </div> <div class="clear"></div>
 <div class="home_category_list">
 <ul class="prolist">
 <li>
 <div class="pro-tu">
-<a href="https://www.vivishop.tw/xiushen-niuzaiku.html"><img src="picture/2_1_8.jpg" width="400" height="400" alt=""/></a>
+<a href="https://www.vivishop.tw/xiushen-niuzaiku.html"><img src="img/zlt.jpg" width="400" height="400" alt=""/></a>
 </div>
 <div class="pro-tex">
 <h3><a href="https://www.vivishop.tw/xiushen-niuzaiku.html">新款女修身小腳牛仔褲 【加$400再來一件】</a></h3>
@@ -79,7 +79,7 @@
 </li>
 <li>
 <div class="pro-tu">
-<a href="https://www.vivishop.tw/500-59678.html"><img src="picture/711ok51h722k55y9g2gu981p281_1_.jpg" width="400" height="400" alt=""/></a>
+<a href="https://www.vivishop.tw/500-59678.html"><img src="img/zlt.jpg" width="400" height="400" alt=""/></a>
 </div>
 <div class="pro-tex">
 <h3><a href="https://www.vivishop.tw/500-59678.html">韓版氣質針織網紗拼接連衣裙【第二件僅$500】</a></h3>
@@ -91,7 +91,7 @@
 </li>
 <li>
 <div class="pro-tu">
-<a href="https://www.vivishop.tw/v-1381.html"><img src="picture/4n15t515op76t4g83wt1uui1b601.jpg" width="400" height="400" alt=""/></a>
+<a href="https://www.vivishop.tw/v-1381.html"><img src="img/zlt.jpg" width="400" height="400" alt=""/></a>
 </div>
 <div class="pro-tex">
 <h3><a href="https://www.vivishop.tw/v-1381.html">【爆紅款】時尚V領長袖針織開衫【第二件僅需$300】</a></h3>
@@ -103,7 +103,7 @@
 </li>
 <li>
 <div class="pro-tu">
-<a href="https://www.vivishop.tw/poscilla-500.html"><img src="picture/o15ww9w6o17m55oz071uqwr658banner2.jpg" width="400" height="400" alt=""/></a>
+<a href="https://www.vivishop.tw/poscilla-500.html"><img src="img/zlt.jpg" width="400" height="400" alt=""/></a>
 </div>
 <div class="pro-tex">
 <h3><a href="https://www.vivishop.tw/poscilla-500.html">POSCILLA針織拼接連衣裙【第二件僅售$500】</a></h3>
@@ -115,7 +115,7 @@
 </li>
 <li>
 <div class="pro-tu">
-<a href="https://www.vivishop.tw/400-59657.html"><img src="picture/006a73f19cd4614d8b1f9643cd91f57b.jpg" width="400" height="400" alt=""/></a>
+<a href="https://www.vivishop.tw/400-59657.html"><img src="img/zlt.jpg" width="400" height="400" alt=""/></a>
 </div>
 <div class="pro-tex">
 <h3><a href="https://www.vivishop.tw/400-59657.html">高腰甜美雪紡網紗百褶裙【第二件僅400】</a></h3>
@@ -127,7 +127,7 @@
 </li>
 <li>
 <div class="pro-tu">
-<a href="https://www.vivishop.tw/cotton-and-linen-pants.html"><img src="picture/f70a2dfc933f46cd13be90160b55b6e3.jpg" width="400" height="400" alt=""/></a>
+<a href="https://www.vivishop.tw/cotton-and-linen-pants.html"><img src="img/zlt.jpg" width="400" height="400" alt=""/></a>
 </div>
 <div class="pro-tex">
 <h3><a href="https://www.vivishop.tw/cotton-and-linen-pants.html">日系薄款冰絲棉麻小脚褲，視覺增高5cm！【三色可選】</a></h3>
@@ -181,7 +181,7 @@
 <ul class="prolist">
 <li>
 <div class="pro-tu">
-<a href="https://www.vivishop.tw/xichenqi.html"><img src="picture/xcq-1.jpg" width="400" height="400" alt=""/></a>
+<a href="https://www.vivishop.tw/xichenqi.html"><img src="img/zlt.jpg" width="400" height="400" alt=""/></a>
 </div>
 <div class="pro-tex">
 <h3><a href="https://www.vivishop.tw/xichenqi.html">四合一乾濕兩用車用吸塵器【三折激殺】</a></h3>
@@ -193,7 +193,7 @@
 </li>
 <li>
 <div class="pro-tu">
-<a href="https://www.vivishop.tw/lipstick-microphone.html"><img src="picture/g15s4ys17u64v3bpgphi60z5r275001.jpg" width="400" height="400" alt=""/></a>
+<a href="https://www.vivishop.tw/lipstick-microphone.html"><img src="img/zlt.jpg" width="400" height="400" alt=""/></a>
 </div>
 <div class="pro-tex">
 <h3><a href="https://www.vivishop.tw/lipstick-microphone.html">mr.leaf迷你口紅唱吧麥克風【第二隻僅需300！】</a></h3>
@@ -206,7 +206,7 @@
 </ul>
 <div class="clear"></div>
 </div>
-<div id="load" style="width:100%;text-algin:center;height:40px;padding:8px 0;"><img src="picture/loading.gif" style="width:auto;margin:0 auto;display:none;"></div>
+<div id="load" style="width:100%;text-algin:center;height:40px;padding:8px 0;"><img src="images/loading.gif" style="width:auto;margin:0 auto;display:none;"></div>
 
 <style>#descDiv .prolist li{padding:5px;box-sizing:border-box;border:0;box-shadow:none;display:none;}#descDiv .prolist li .newprice{font-weight:normal;}#descDiv .prolist li .newprice .fprice{font-style:normal;font-size:18px;}</style>
 
@@ -223,10 +223,35 @@ catch(e){}
 var bullets=document.getElementById('position').getElementsByTagName('li');var banner=Swipe(document.getElementById('mySwipes'),{auto:4000,continuous:true,disableScroll:false,callback:function(pos){var i=bullets.length;while(i--){bullets[i].className=' ';}bullets[pos].className='cur';}})
 </script>
 <script>
-jQuery(document).ready(function(){var state=true;var linum =jQuery("#descDiv ul li").length;jQuery("#descDiv ul li:lt(4)").show();jQuery(window).scroll(function(){var scrot=jQuery(document).scrollTop()+100;if (scrot >=jQuery(document).height() - jQuery(window).height()) {if(state==true){state=false;jQuery("#load img").css("display","block");setTimeout(function(){jQuery("#load img").css("display","none");var lilen =jQuery("#descDiv ul li:visible").length;var lilent =lilen + 4;jQuery("#descDiv ul li:lt("+lilent+")").show();if(lilent >=linum){jQuery("#load").html("<p style='text-align:center;line-height:30px;font-size:14px;'>已經到最底端了</p>").css({"margin-top":"1px"});}else{state=true;}
-},1000);}
-}
-});});
+    var site_id = {{ $site->sites_id }}
+jQuery(document).ready(function($){
+    var state=true;
+    var page =1;
+    var linum =jQuery("#descDiv ul li").length;
+    jQuery("#descDiv ul li:lt(4)").show();
+    jQuery(window).scroll(function(){
+        var scrot=jQuery(document).scrollTop()+100;
+        if (scrot >=jQuery(document).height() - jQuery(window).height()) {
+            if(state==true){
+                state=false;
+                jQuery("#load img").css("display","block");
+                $.ajax({
+                    type:'get',
+                    url:'/index/get_site_goods?site_id='+site_id+'page='+page+'active_type=1',
+                    success:function(data){
+                        
+                    }
+                });
+                setTimeout(function(){
+                    jQuery("#load img").css("display","none");
+                    var lilen =jQuery("#descDiv ul li:visible").length;
+                    var lilent =lilen + 4;jQuery("#descDiv ul li:lt("+lilent+")").show();
+                    if(lilent >=linum){
+                        jQuery("#load").html("<p style='text-align:center;line-height:30px;font-size:14px;'>已經到最底端了</p>").css({"margin-top":"1px"});
+                    }else{state=true;}
+               },1000);}
+        }
+ });});
 </script>
 @endsection
 
