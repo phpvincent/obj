6c3920028bc22f86246531f2c73fd38cd96fc6db
inono1991 <2998604835@qq.com>
2019-02-26 11:11:57 +0800

    ''

--- app/Jobs/SendHerbEmail.php
+++ app/Jobs/SendHerbEmail.php
@@ -9,6 +9,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use App\order;
 use App\url;
+
 class SendHerbEmail implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
@@ -61,7 +62,12 @@ class SendHerbEmail implements ShouldQueue
            }
            if(strpos($url,'/index/site_goods/')!== false){
               $site_id=\App\site::where('sites_blade_type',$goods->goods_blade_type)->first()['sites_id'];
-              $home_url=url::where('url_site_id',$site_id)->first()['url_url'];
+              if($site_id){
+                  $home_url=url::where('url_site_id',$site_id)->first()['url_url'];
+              }else{
+                  $home_url=url::where('url_site_id','>', 0)->first()['url_url'];
+              }
+
            }else{
               $home_url=$url;
            }
@@ -79,7 +85,7 @@ class SendHerbEmail implements ShouldQueue
                 $goods->img = $imgs[$mycount];
             }else{
                 $goods->img = '';
-            } 
+            }
             //拼装订单属性信息
                $order_config=\App\order_config::where('order_primary_id',$order->order_id)->get();
                     if($order_config->count()>0){
