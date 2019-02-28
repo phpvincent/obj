4ce19883d77dc546836e947467f1394c04f52bbc
zhang <18238205232@163.com>
2019-02-28 10:19:02 +0800

    cui 02-28

--- resources/views/admin/kind/index.blade.php
+++ resources/views/admin/kind/index.blade.php
@@ -66,7 +66,7 @@
             "order": [[ 0, "desc" ]],
             "stateSave": false,
             "columnDefs": [{
-                "targets": [0,2,3,4,5,6,7,8,9,10,11,12],
+                "targets": [1,2,3,4,5,6,7,8,9,10,11,12],
                 "orderable": false
             }],
             "processing": true,
