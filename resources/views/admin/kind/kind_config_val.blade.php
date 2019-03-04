c4563533b8e12203d18d5d6068f2c96f5dac4d82
zhang <18238205232@163.com>
2019-03-04 09:17:40 +0800

    cui 03-14

--- resources/views/admin/kind/kind_config_val.blade.php
+++ resources/views/admin/kind/kind_config_val.blade.php
@@ -699,7 +699,7 @@
                                 //回调
                                 index = parent.layer.getFrameIndex(window.name);
                                 setTimeout("parent.layer.close(index);",100);
-                                parent.shuaxin(); 
+                                parent.shuaxin();
                             });
                         }else{
                             layer.msg(data.msg);
