c2bba209882e845c05dc450803201894a4316a60
vincent <phpvincent@163.com>
2019-03-14 17:36:52 +0800

    jsq

--- resources/views/storage/storage/jsq.blade.php
+++ resources/views/storage/storage/jsq.blade.php
@@ -1,5 +1,15 @@
 @extends('storage.father.static')
 @section('content')
+<style type="text/css">
+	
+</style>
+<div>
+
+</div>
+
 @endsection
 @section('js')
+<script type="text/javascript">
+	
+</script>
 @endsection
