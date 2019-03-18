f2b82fd09e8beb1ba59f0098e1a7c724ef889ecd
fangaige <251160876@qq.com>
2019-03-18 16:57:13 +0800

    addgoods完成

--- resources/views/storage/add/add_goods.blade.php
+++ resources/views/storage/add/add_goods.blade.php
@@ -12,6 +12,7 @@
                                     <label class="layui-form-label">搜索选择框</label>
                                     <div class="layui-input-inline">
                                         <select name="goods_kind" lay-verify="required" lay-search="" lay-filter="goodsSelec">
+                                        <option value="">请选择</option>
                                             @foreach($product as $item)
                                             <option value="{{$item->goods_kind_id}}">{{$item->goods_kind_name}}</option>
                                             @endforeach
@@ -21,13 +22,14 @@
                             </div>
                             <div class="layui-form-item">
                                <label class="layui-form-label">选择变种:</label>
+                               <input type="checkbox" name="" title="全选" lay-filter="allGoodsCheck">
                             </div>
                             <div class="layui-form-item">
                                 <div class="layui-inline">
-                                    <div class="layui-input-block">
-                                        <input type="checkbox" name="" title="豆豆" goods_sku="1" goods_kind_name="豆豆" good_attr="红大" lay-filter="goodsCheck">
+                                    <div class="goodsCheckbox">
+                                        <!-- <input type="checkbox" name="" title="豆豆" goods_sku="1" goods_kind_name="豆豆" good_attr="红大" lay-filter="goodsCheck">
                                         <input type="checkbox" name="" title="香水" goods_sku="2" goods_kind_name="香水" good_attr="白大" lay-filter="goodsCheck"> 
-                                        <input type="checkbox" name="" title="牛仔" goods_sku="3" goods_kind_name="牛仔" good_attr="小女" lay-filter="goodsCheck"> 
+                                        <input type="checkbox" name="" title="牛仔" goods_sku="3" goods_kind_name="牛仔" good_attr="小女" lay-filter="goodsCheck">  -->
                                     </div>
                                 </div>
                             </div>
@@ -37,20 +39,67 @@
                 <div class="layui-card">
                 <div class="layui-card-header">补货单</div>
                 <div class="layui-card-body" pad15>
-                   <form class="layui-form layui-form-pane" method="post" lay-filter="" action="">
+                   <form class="layui-form layui-form-pane goodsAppendForm" method="post" lay-filter="" action="">
                    <div class="layui-row">
-                       <div class="layui-col-md6">
-                         laofan 
-                       </div>
-                       <div class="layui-col-md6">
-                         laofan 
-                       </div>
+                      <table class="layui-table goodsAppend">
+                        <thead>
+                          <tr>
+                           <th>产品ID</th>
+                            <th>产品名称</th>
+                            <th>变种</th>
+                            <th>SKU</th>
+                            <th>数量</th>
+                            <th>操作</th>
+                          </tr> 
+                        </thead>
+                        <tbody>
+
+                        </tbody>
+                      </table>
+                    </div>
+                    <div class="layui-row">
+                        <div class="layui-form-item">
+                           <div class="layui-input-block">
+                             <button style="float: right;" class="layui-btn" lay-submit lay-filter="formDemo">立即提交</button>
+                           </div>
+                        </div>
                     </div>
                    </form>
                 </div>
             </div>
         </div>
     </div>
+   <script id="goodsCheckbox" type="text/html">
+     @{{#  layui.each(d.data, function(index, item){ }}
+     <input type="checkbox" name="" title="@{{item.goods_attr}}" goods_sku="@{{item.goods_sku}}" goods_kind_id="@{{item.goods_kind_id}}" goods_kind_name="@{{item.goods_kind_name}}" goods_attr="@{{item.goods_attr}}" lay-filter="goodsCheck">
+     @{{#  }); }}
+   </script>
+   <script id="goodsAppend" type="text/html">
+        <tr>
+          <td>
+          <input type="hidden" readonly name="goods_kind_id" value="@{{d.goods_kind_id}}" class="layui-input">
+          <span>@{{d.goods_kind_id}}</span>
+          </td>
+          <td>
+          <input type="hidden" readonly name="goods_kind_name" value="@{{d.goods_kind_name}}" class="layui-input">
+          <span>@{{d.goods_kind_name}}</span>
+          </td>
+          <td>
+          <input type="hidden" readonly name="goods_attr" value="@{{d.goods_attr}}" class="layui-input">
+          <span>@{{d.goods_attr}}</span>
+          </td>
+          <td>
+          <input type="hidden" readonly name="goods_sku" value="@{{d.goods_sku}}" class="layui-input">
+          <span>@{{d.goods_sku}}</span>
+          </td>
+          <td>
+          <input type="text" name="num" value="" placeholder="请输入数量" class="layui-input" lay-verify="required">
+          </td>
+          <td>
+           <span class="layui-btn layui-btn-xs layui-btn-danger removeGoodsAppend"><i class="layui-icon">&#xe640;</i></span>
+          </td>
+        </tr>
+   </script>
 @endsection
 @section('js')
     <script>
@@ -58,50 +107,109 @@
             base: '{{asset("/admin/layuiadmin/")}}/' //静态资源所在路径
         }).extend({
             index: 'lib/index' //主入口模块
-        }).use(['form','index', 'set','admin'],function(){
+        }).use(['form','index', 'set', 'admin', 'laytpl'],function(){
             var form=layui.form;
-
+            var laytpl = layui.laytpl;
             var admin=layui.admin;
             var $=layui.jquery;
+            // 监听 下拉框变化
             form.on('select(goodsSelec)', function(data) {
                console.log('goodsSelec',data)
+               var index = layer.load();
                $.ajax({
-                   type: "get",
-                   url: "url",
-                   data: data.value,
+                   type:'post',
+                   url: "get_goods_config",
+                   data: {id:data.value},
+                   headers: { 'X-CSRF-TOKEN' : '{{ csrf_token() }}' },
                 //    dataType: "dataType",
                    success: function (response) {
+                       console.log('response',response)
+                       layer.close(index);
+                       var getTpl = goodsCheckbox.innerHTML
+                      laytpl(getTpl).render(response, function(string){
+                         $('.goodsCheckbox').html(string)
+                         form.render('checkbox');
+                       });
+                      // 让之前已经选过的重新选中；
+                      var datas = $(".goodsAppendForm").serializeArray()
+                      $.each(datas, function (index, value) { 
+                          if (value.name === 'goods_sku') {
+                            $('.goodsCheckbox input[goods_sku="'+value.value+'"]').prop("checked", true)
+                            form.render('checkbox');
+                          }
+                      });
                        
                    }
                });
             })
-
+            // 复选框监听
             form.on('checkbox(goodsCheck)', function(data) {
-               console.log('goodsCheck',data)
-               console.log('checked',data.elem.checked,$(data.elem).prop("title"),$(data.elem).attr("goods_sku"),$(data.elem).attr("goods_kind_name"), $(data.elem).attr("good_attr") )
+               var datas = {};
+               datas['goods_kind_name']=$(data.elem).attr("goods_kind_name")
+               datas['goods_sku']=$(data.elem).attr("goods_sku")
+               datas['goods_attr']=$(data.elem).attr("goods_attr")
+               datas['goods_kind_id']=$(data.elem).attr("goods_kind_id")
+               if(data.elem.checked) {
+                  var getTpl = goodsAppend.innerHTML
+                  laytpl(getTpl).render(datas, function(string){
+                     console.log(string);
+                     $('.goodsAppend tbody').append(string)
+                   });
+               } else {
+                   $('.goodsAppend tbody input[value="'+datas.goods_sku+'"]').parent().parent().remove()
+               }
             })
-
-            var addressID = $("input[name='is_local']:checked").val();
-            if(addressID === '1'){
-                $('.item-model-hide').addClass('layui-hide');
-            }
-
-            form.on('radio(is_local)', function () {
-                var addressID = $("input[name='is_local']:checked").val();
-                if(addressID === '1'){
-                    $('.item-model-hide').addClass('layui-hide');
-                }else{
-                    $('.item-model-hide').removeClass('layui-hide');
+            // checkbox全选按钮
+            form.on('checkbox(allGoodsCheck)', function(data) {
+                if(data.elem.checked){
+                    $(".goodsCheckbox input").prop("checked", true);
+                    form.render('checkbox');
+                    $.each($(".goodsCheckbox input"), function (index, value) { 
+                        var datas = {};
+                        datas['goods_kind_name']=$(value).attr("goods_kind_name")
+                        datas['goods_sku']=$(value).attr("goods_sku")
+                        datas['goods_attr']=$(value).attr("goods_attr")
+                        datas['goods_kind_id']=$(value).attr("goods_kind_id")
+                        var getTpl = goodsAppend.innerHTML
+                        laytpl(getTpl).render(datas, function(string){
+                           $('.goodsAppend tbody').append(string)
+                          })
+                    })
+                } else {
+                    $(".goodsCheckbox input").prop("checked", false);
+                    form.render('checkbox');
+                    $.each($(".goodsCheckbox input"), function (index, value) { 
+                        $('.goodsAppend tbody input[value="'+$(value).attr("goods_sku")+'"]').parent().parent().remove()
+                    })
                 }
-            });
-
-            form.on('submit',function(data){
+            })
+            // 表格提交
+            form.on('submit(formDemo)', function(data){
+              console.log(data.field);
+              console.log($(data.form).serializeArray());
+              var data = $(data.form).serializeArray();
+              var num = 0;
+              var obj = {};
+              var arr = [];
+              for(var i=0; i<data.length; i++){
+                 if(num===4){
+                    obj[data[i].name] = data[i].value
+                    arr.push(obj)
+                    num = 0;
+                    obj = {};
+                 } else{
+                   obj[data[i].name] = data[i].value
+                   num++
+                 }
+              }
+              console.log(arr);
                 var index = layer.load();
                 $.ajax({
-                    url:"{{url('admin/storage/list/add_storage')}}",
+                    url:"/admin/storage/add/add_goods",
                     type:'post',
-                    data:data.field,
-                    datatype:'json',
+                    data:JSON.stringify(arr),
+                    // datatype:'json',
+                    headers: { 'X-CSRF-TOKEN' : '{{ csrf_token() }}' },
                     success:function(msg){
                         if(msg['err']==1){
                             layer.close(index);
@@ -120,7 +228,13 @@
                         }
                     }
                 });
-                return false;
+              return false;
+            });
+            // 表格里面的删除按钮
+            $(document).on("click",".removeGoodsAppend",function () {
+               $(this).parent().parent().remove()
+               $('.goodsCheckbox input[goods_sku="'+$(this).parent().parent().find('input[name="goods_sku"]').val()+'"]').prop("checked", false)
+               form.render('checkbox');
             })
         });
 
