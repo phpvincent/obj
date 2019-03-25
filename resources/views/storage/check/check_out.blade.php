f0cb156deca219afdf2858a59203174d14e8b319
phpvincent@163.com <phpvincent@163.com>
2019-03-25 17:02:23 +0800

    扣货信息添加js原生导出表格功能

--- resources/views/storage/check/check_out.blade.php
+++ resources/views/storage/check/check_out.blade.php
@@ -12,7 +12,8 @@
         	<blockquote class="layui-elem-quote">订单编号：{{\App\order::where('order_id',$storage_check_data[0]->storage_check_data_order)->first(['order_single_id'])['order_single_id']}}</blockquote>
         	<blockquote class="layui-elem-quote">校准单编号：{{\App\storage_check::where('storage_check_id',$storage_check_data[0]->storage_primary_id)->first(['storage_check_string'])['storage_check_string']}}</blockquote>
         	<blockquote class="layui-elem-quote" style="color:brown">仓库：{{\App\storage::where('storage_id',$storage_check_data[0]->storage_abroad_id)->first(['storage_name'])['storage_name']}}(海外仓)</blockquote>
-          <table class="layui-table" lay-filter="test">
+					<a download="扣货信息表.xls" id="excelOut" href="#" style="height: 20px;width: 100%;display: block;"><div class="layui-inline" title="导出" lay-event="LAYTABLE_EXPORT" style="float: right;"><i class="layui-icon layui-icon-export"></i></div></a>
+					<table class="layui-table"id="tableExcel" lay-filter="test">
 			  <thead>
 			  	<tr>
 			  		<th rowspan="3">扣货信息</th>
@@ -38,7 +39,8 @@
         	<blockquote class="layui-elem-quote">订单编号：{{\App\order::where('order_id',$storage_check_data[0]->storage_check_data_order)->first(['order_single_id'])['order_single_id']}}</blockquote>
         	<blockquote class="layui-elem-quote">校准单编号：{{\App\storage_check::where('storage_check_id',$storage_check_data[0]->storage_primary_id)->first(['storage_check_string'])['storage_check_string']}}</blockquote>
         	<blockquote class="layui-elem-quote" style="color:brown">仓库：{{\App\storage::where('storage_id',$storage_check_data[0]->storage_abroad_id)->first(['storage_name'])['storage_name']}}(海外仓)</blockquote>
-        	 <table class="layui-table" lay-filter="test">
+					<a download="扣货信息表.xls" id="excelOut" href="#" style="height: 20px;width: 100%;display: block;"><div class="layui-inline" title="导出" lay-event="LAYTABLE_EXPORT" style="float: right;"><i class="layui-icon layui-icon-export"></i></div></a> 
+					<table class="layui-table"id="tableExcel" lay-filter="test">
 			  <thead>
 			  	<tr>
 			  		<th rowspan="3">扣货信息</th>
@@ -64,7 +66,8 @@
         	<blockquote class="layui-elem-quote">订单编号：{{\App\order::where('order_id',$storage_check_data[0]->storage_check_data_order)->first(['order_single_id'])['order_single_id']}}</blockquote>
         	<blockquote class="layui-elem-quote">校准单编号：{{\App\storage_check::where('storage_check_id',$storage_check_data[0]->storage_primary_id)->first(['storage_check_string'])['storage_check_string']}}</blockquote>
         	<blockquote class="layui-elem-quote" style="color:green">仓库：{{\App\storage::where('storage_id',$storage_check_data[0]->storage_abroad_id)->first(['storage_name'])['storage_name']}}(本地仓)</blockquote>
-        	 <table class="layui-table" lay-filter="test">
+					<a download="扣货信息表.xls" id="excelOut" href="#" style="height: 20px;width: 100%;display: block;"><div class="layui-inline" title="导出" lay-event="LAYTABLE_EXPORT" style="float: right;"><i class="layui-icon layui-icon-export"></i></div></a>
+				<table  id="tableExcel" class="layui-table" lay-filter="test">
 			  <thead>
 			  	<tr>
 			  		<th rowspan="3">扣货信息</th>
@@ -85,4 +88,32 @@
 @endif
 @endsection
 @section('js')
+<script>
+window.onload = function () {
+        tableToExcel('tableExcel', '下载模板')
+    };
+    //base64转码
+    var base64 = function (s) {
+        return window.btoa(unescape(encodeURIComponent(s)));
+    };
+    //替换table数据和worksheet名字
+    var format = function (s, c) {
+        return s.replace(/{(\w+)}/g,
+            function (m, p) {
+                return c[p];
+            });
+    }
+    function tableToExcel(tableid, sheetName) {
+        var uri = 'data:application/vnd.ms-excel;base64,';
+        var template = '<html xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:x="urn:schemas-microsoft-com:office:excel"' +
+            'xmlns="http://www.w3.org/TR/REC-html40"><meta charset="utf-8" /><head><!--[if gte mso 9]><xml><x:ExcelWorkbook><x:ExcelWorksheets><x:ExcelWorksheet>'
+            + '<x:Name>{worksheet}</x:Name><x:WorksheetOptions><x:DisplayGridlines/></x:WorksheetOptions></x:ExcelWorksheet></x:ExcelWorksheets>'
+            + '</x:ExcelWorkbook></xml><![endif]-->' +
+            '</head><body ><table class="excelTable">{table}</table></body></html>';
+        if (!tableid.nodeType) tableid = document.getElementById(tableid);
+        var ctx = {worksheet: sheetName || 'Worksheet', table: tableid.innerHTML};
+        document.getElementById("excelOut").href = uri + base64(format(template, ctx));
+    }
+
+</script>
 @endsection
