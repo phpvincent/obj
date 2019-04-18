685f35f521cbe0db469c275e0112443ca0e2de75
vincent <phpvincent@163.com>
2019-04-18 18:51:06 +0800

    fix

--- resources/views/home/TaiwanJian/endsuccess.blade.php
+++ resources/views/home/TaiwanJian/endsuccess.blade.php
@@ -18,6 +18,7 @@
 <link href="/css/pay.css" rel="stylesheet">
 <link href="/css/JS5.css" rel="stylesheet" type="text/css">
 <link href="/css/page-success.css" rel="stylesheet" type="text/css">
+  <script type="text/javascript" src="/js/jquery.min.js"></script>
   <script type="text/javascript" src="/js/moudul/websockets.js?v=1.0"></script>
 @if($order['pix_event'])
       @if($goods->goods_pix!=null&&$goods->goods_pix!='')    
