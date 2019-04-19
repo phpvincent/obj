4f9d0f5567f9036cf9db062fca262ae30d31c5e4
vincent <phpvincent@163.com>
2019-04-19 18:06:40 +0800

    fix

--- resources/views/home/TaiwanJian/endsuccess.blade.php
+++ resources/views/home/TaiwanJian/endsuccess.blade.php
@@ -18,8 +18,11 @@
 <link href="/css/pay.css" rel="stylesheet">
 <link href="/css/JS5.css" rel="stylesheet" type="text/css">
 <link href="/css/page-success.css" rel="stylesheet" type="text/css">
-  <script type="text/javascript" src="/js/jquery.min.js"></script>
-  <script type="text/javascript" src="/js/moudul/websockets.js?v=1.0"></script>
+  
+  @if($is_monitor==1&&in_array('6',explode(',',\App\worker_monitor::first(['worker_monitor_route_type'])['worker_monitor_route_type'])))
+            <script type="text/javascript" src="/js/jquery.min.js"></script>
+            <script type="text/javascript" src="/js/moudul/websockets.js?v=1.0"></script>
+        @endif
 @if($order['pix_event'])
       @if($goods->goods_pix!=null&&$goods->goods_pix!='')    
     <!-- Facebook Pixel Code -->
