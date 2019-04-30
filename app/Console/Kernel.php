ebfec1d6cd34ace5f85ad91bcff5e9cc2878b667
vincent <phpvincent@163.com>
2019-04-30 17:52:52 +0800

    fix

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -28,30 +28,30 @@ class Kernel extends ConsoleKernel
         //          ->hourly();
                  $filePath='./time.log';
                  $schedule->call(function(){
-                   check_pay_order();    
+                   //check_pay_order();    
                  })->everyMinute()->appendOutputTo($filePath);
                  $schedule->call(function(){
-                    get_new_currency_rate();
+                    //get_new_currency_rate();
                    /* \Log::notice('check');*/
                  })->dailyAt('3:00');
                  $schedule->call(function(){
-                    get_browse_info();
+                    //get_browse_info();
                    /* \Log::notice('check');*/
                  })->dailyAt('23:59');
                  $schedule->call(function(){
-                    sendMessage::message_notice();
+                    //sendMessage::message_notice();
                  })->hourly();
                  /**
                   * 定时进行仓储数据校对
                   */
                  $schedule->call(function(){
-                    auto_storage_check();
+                    //auto_storage_check();
                  })->everyThirtyMinutes();
                   /**
                   * 定时监控网站状态
                   */
                  $schedule->call(function(){
-                    check_web_status();
+                    //check_web_status();
                  })->everyTenMinutes();
                  //定时发送订单核审通知
                  $schedule->call(function(){
