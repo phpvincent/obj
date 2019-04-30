0fa2f9f27697707f798d0d00bd7e83aaf5ecfed2
vincent <phpvincent@163.com>
2019-04-30 18:28:49 +0800

    fix

--- app/channel/sendMessage.php
+++ app/channel/sendMessage.php
@@ -76,7 +76,7 @@ class sendMessage{
              $SendSmsApi=new \SendSmsApi();
              \Log::notice($phone.'------'.$str.'-----------'.env('FASTOO_APIKEY','e40a68e8ad4d4ea4b4578d1d658f3ecd'));
              if(substr($phone, 0,2)=='86'){
-                $str='【ydzsshop】'.$str;
+                $str='zsshop notice:'.$str;
              }
              $bean=$SendSmsApi->Submit(env('FASTOO_APIKEY','e40a68e8ad4d4ea4b4578d1d658f3ecd'), $phone, $str);
              \Log::notice(json_encode($bean));
