55d9d582390ac43823d50c07e480c69ca7445679
vincent <phpvincent@163.com>
2019-05-08 09:27:18 +0800

    fix

--- public/js/moudul/websockets.js
+++ public/js/moudul/websockets.js
@@ -116,7 +116,7 @@ var wsArr = (function(){
 
 //var wsUri ="ws://192.168.10.166:2349/";
 var wsUri ="ws://13.229.73.221:2349/";
-// var wsUri ="ws://192.168.10.10:2349/";
+ //var wsUri ="ws://192.168.10.166:2349/";
 var  heartbeat=null
     function testWebSocket() { 
         websocket = new WebSocket(wsUri); 
@@ -150,8 +150,6 @@ clearInterval(heartbeat)
   function onClose(evt) { 
       console.log("关闭ws"); 
       clearInterval(heartbeat)
-      //断线重连
-      testWebSocket()
   }  
 
   function onMessage(evt) { 
