6266d1f1caa1b9e63cee3097069768c5f410902d
fangaige <251160876@qq.com>
2019-05-10 18:00:55 +0800

    优惠券限制 字体变小

--- public/js/moudul/functMoudul.js
+++ public/js/moudul/functMoudul.js
@@ -551,7 +551,7 @@ var msg =function(el){
 // 优惠券是否可用
 var subtSatisfy =function(el){
     if(el.goods_cheap_type==='2'){
-        return cheapSatisfy+'<span>'+moneycoin+el.goods_cheap_remark+'</span>'
+        return cheapSatisfy+'<span style="font-size: 14px;">'+moneycoin+el.goods_cheap_remark+'</span>'
     }else{
         return cheapSatisfyNone
     }
